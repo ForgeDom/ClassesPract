45b0517dfd24d3e9adaa159c271569e5f4407d3b
Max <maxstill2006@gmail.com>
2025-03-21 10:00:41 +0200

    +

--- ClassesPract/Program.cs
+++ ClassesPract/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("Enter array of numbers from 0 to 9:");
+        string input = Console.ReadLine();
+
+        try
+        {
+            int number = Convert.ToInt32(input);
+            Console.WriteLine($"Converted number: {number}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Input is not a number");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Number is too big");
+        }
     }
 }
